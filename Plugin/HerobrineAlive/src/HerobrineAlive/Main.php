dc3b69d23d05193472e9eb4093666d4e14bd90eb
Philip Shilling <comedyman937@yahoo.com>
2014-06-08 14:34:23 -0500

    Added main command features

--- Plugin/HerobrineAlive/src/HerobrineAlive/Main.php
+++ Plugin/HerobrineAlive/src/HerobrineAlive/Main.php
@@ -9,11 +9,31 @@ use pocketmine\Player;
 
 class Main extends PluginBase implements Listener, CommandExecutor{
     public funtion onEnable(){
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
         $this->getLogger()->log("[INFO] HerobrineAlive Loaded!");
     }
     
-    public function onCommand(){
-        //HerobrineAlive Commands
+    public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
+        switch($cmd->getName()){
+            case "herobrinealive":
+                if($args[0] == "release"){
+                    //Start HerobrineAI()
+                    $sender->sendMessage("[HerobrineAlive] Herobrine has been released!");
+                }elseif($args[0] == "kill"){
+                    //Stop HerobrineAI() and remove his entity packets
+                    $sender->sendMessage("[HerobrineAlive] Herobrine has been killed!");
+                }elseif($args[0] == "attack"){
+                    if($args[1] == instance of player){
+                        //Have Herobrine go after $args[1]
+                        $sender->sendMessage("[HerobrineAlive] Herobrine is now after " . $args[1] . "!");
+                    }else{
+                        $sender->sendMessage("[HerobrineAlive] Target player not found!");
+                    }
+                }else{
+                    $sender->sendMessage("Usage: /HerobrineAlive <RELEASE|KILL|ATTACK> [PLAYER]");
+                }
+            break;
+        }
     }
     
     public function HerobrineAI(){

4751dc1866fca45e04aa3b4f3e6dc1a79e9182b6
Philip Shilling <comedyman937@yahoo.com>
2014-06-08 21:24:11 -0500

    Fixed Typo

--- Plugin/HerobrineAlive/src/HerobrineAlive/Main.php
+++ Plugin/HerobrineAlive/src/HerobrineAlive/Main.php
@@ -10,7 +10,7 @@ use pocketmine\event\Listener;
 use pocketmine\Player;
 
 class Main extends PluginBase implements Listener, CommandExecutor{
-    public funtion onEnable(){
+    public function onEnable(){
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
         $this->getLogger()->log("[INFO] HerobrineAlive Loaded!");
     }

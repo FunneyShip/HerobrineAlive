b9dbe27d24fd56fa8ad93c7696829d5a70a1ebb0
Philip Shilling <comedyman937@yahoo.com>
2014-06-08 20:49:14 -0500

    Create StartHerobrineAI.php

---
+++ Plugin/HerobrineAlive/src/HerobrineAlive/StartHerobrineAI.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace HerobrineAlive;
+
+use pocketmine\scheduler\PluginTask;
+use pocketmine\Player;
+
+class StartHerobrineAI extends PluginTask{
+    public function onRun(){
+        /*
+         * Create Herobrine
+         */
+    }
+}
